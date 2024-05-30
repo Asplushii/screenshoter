41b3e4a121a451a517c6fde8449c24f52223f499
Asplushi_ <veis2k15veis@gmail.com>
2024-05-30 15:53:37 +0200

    idk i just wrote code

--- main.c
+++ main.c
@@ -1,135 +1,77 @@
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-#include <X11/Xatom.h>
-#include <png.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xatom.h>
+#include <png.h>
 #include <omp.h>
 
-#define ERROR_FILE_OPEN 2
-#define ERROR_PNG_STRUCT 3
-#define ERROR_PNG_INFO 4
-#define ERROR_PNG_CREATION 5
-#define ERROR_CAPTURE_SCREEN 6
-#define ERROR_DISPLAY_OPEN 9
-
-void handle_error(const char *error_message, int exit_code) {
-    fprintf(stderr, "Error: %s\n", error_message);
-    exit(exit_code);
+void handle_error(const char *msg, int code) {
+    fprintf(stderr, "Error: %s\n", msg);
+    exit(code);
 }
 
-XImage *capture_screen(Display *display, Window window) {
-    XWindowAttributes window_attributes;
-    if (!XGetWindowAttributes(display, window, &window_attributes)) {
-        handle_error("Failed to get window attributes", ERROR_CAPTURE_SCREEN);
-    }
-    return XGetImage(display, window, 0, 0, window_attributes.width, window_attributes.height, AllPlanes, ZPixmap);
+XImage *capture_screen(Display *d, Window w) {
+    XWindowAttributes a;
+    XGetWindowAttributes(d, w, &a);
+    return XGetImage(d, w, 0, 0, a.width, a.height, AllPlanes, ZPixmap);
 }
 
-void save_image_to_png(XImage *img, const char *filename) {
-    FILE *fp = fopen(filename, "wb");
-    if (!fp) {
-        handle_error("Could not open file for writing", ERROR_FILE_OPEN);
-    }
-
+void save_image_to_png(XImage *i, const char *f) {
+    FILE *fp = fopen(f, "wb");
     png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (!png) {
-        fclose(fp);
-        handle_error("png_create_write_struct failed", ERROR_PNG_STRUCT);
-    }
-
     png_infop info = png_create_info_struct(png);
-    if (!info) {
-        png_destroy_write_struct(&png, NULL);
-        fclose(fp);
-        handle_error("png_create_info_struct failed", ERROR_PNG_INFO);
-    }
-
-    if (setjmp(png_jmpbuf(png))) {
-        png_destroy_write_struct(&png, &info);
-        fclose(fp);
-        handle_error("Error during PNG creation", ERROR_PNG_CREATION);
-    }
-
     png_init_io(png, fp);
-    png_set_IHDR(png, info, img->width, img->height, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
-
-    png_bytep row_pointers[img->height];
-    png_byte *image_data = malloc(img->width * img->height * 3 * sizeof(png_byte));
-    if (!image_data) {
-        png_destroy_write_struct(&png, &info);
+    png_set_IHDR(png, info, i->width, i->height, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+    png_write_info(png, info);
+    png_byte *pixel_data = (png_byte *)malloc(i->width * i->height * 3 * sizeof(png_byte));
+    if (!pixel_data) {
         fclose(fp);
-        handle_error("Memory allocation failed", ERROR_PNG_CREATION);
+        png_destroy_write_struct(&png, &info);
+        handle_error("Failed to allocate memory", 1);
     }
 
-#pragma omp parallel for
-    for (int y = 0; y < img->height; y++) {
-        for (int x = 0; x < img->width; x++) {
-            unsigned long pixel = XGetPixel(img, x, y);
-            image_data[(y * img->width + x) * 3] = (pixel >> 16) & 0xFF;
-            image_data[(y * img->width + x) * 3 + 1] = (pixel >> 8) & 0xFF;
-            image_data[(y * img->width + x) * 3 + 2] = pixel & 0xFF;
+    #pragma omp parallel for
+    for (int y = 0; y < i->height; y++) {
+        for (int x = 0; x < i->width; x++) {
+            unsigned long p = XGetPixel(i, x, y);
+            int offset = (y * i->width + x) * 3;
+            pixel_data[offset] = (p >> 16) & 0xFF;
+            pixel_data[offset + 1] = (p >> 8) & 0xFF;
+            pixel_data[offset + 2] = p & 0xFF;
         }
     }
 
-    for (int y = 0; y < img->height; y++) {
-        row_pointers[y] = &image_data[y * img->width * 3];
+    for (int y = 0; y < i->height; y++) {
+        png_write_row(png, &pixel_data[y * i->width * 3]);
     }
 
-    png_set_rows(png, info, row_pointers);
-    png_write_png(png, info, PNG_TRANSFORM_IDENTITY, NULL);
-
-    png_destroy_write_struct(&png, &info);
+    png_write_end(png, NULL);
     fclose(fp);
-    free(image_data);
+    free(pixel_data);
 }
 
-void screenshot_whole_screen(Display *display) {
-    XImage *img = capture_screen(display, DefaultRootWindow(display));
-    if (img) {
-        save_image_to_png(img, "/tmp/screenshot_whole_screen.png");
-        XDestroyImage(img);
-        system("xclip -selection clipboard -t image/png -i /tmp/screenshot_whole_screen.png");
-        system("notify-send -i /tmp/screenshot_whole_screen.png 'Screenshot' 'Screenshot saved and copied to clipboard'");
-    } else {
-        handle_error("Failed to capture screen", ERROR_CAPTURE_SCREEN);
-    }
+void screenshot_whole_screen(Display *d) {
+    XImage *i = capture_screen(d, DefaultRootWindow(d));
+    save_image_to_png(i, "/tmp/screenshot.png");
+    XDestroyImage(i);
+    system("xclip -selection clipboard -t image/png -i /tmp/screenshot.png");
+    system("notify-send -i /tmp/screenshot.png 'Screenshot' 'Screenshot saved and copied to clipboard'");
 }
 
 int main(int argc, char *argv[]) {
-    int delay = 0;
-
-    if (argc == 1) {
-
-        printf("Usage:\n");
-        printf("./main --now: Take a screenshot now.\n");
-        printf("./main --inX: Take a screenshot after X seconds.\n");
-        return 0;
-    }
-
+    int d = 0;
+    if (argc == 1) handle_error("Invalid argument", 1);
     if (strncmp(argv[1], "--in", 4) == 0) {
-        delay = atoi(argv[1] + 4);
-        if (delay <= 0) {
-        }
-    } else if (strcmp(argv[1], "--now") != 0) {
-
-        fprintf(stderr, "Error: Invalid argument.\n");
-        return 1;
-    }
-
+        d = atoi(argv[1] + 4);
+        if (d <= 0) handle_error("Invalid delay value", 1);
+    } else if (strcmp(argv[1], "--now") != 0) handle_error("Invalid argument", 1);
     Display *display = XOpenDisplay(NULL);
-    if (!display) {
-        handle_error("Failed to open display", ERROR_DISPLAY_OPEN);
-    }
-
-    if (delay > 0) {
-        sleep(delay);
-    }
-
+    if (!display) handle_error("Failed to open display", 1);
+    if (d > 0) sleep(d);
     screenshot_whole_screen(display);
-
     XCloseDisplay(display);
     return 0;
 }
